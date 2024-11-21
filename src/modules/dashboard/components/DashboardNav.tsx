15ef2b9618530ae90aa651f90ad934c4a4a85537
Hyejin Kim <hjqueeen@gmail.com>
2024-11-21 09:06:44 +0100

    Add nav tree and color

--- src/modules/dashboard/components/DashboardNav.tsx
+++ src/modules/dashboard/components/DashboardNav.tsx
@@ -15,26 +15,26 @@ type ExtendedTreeItemProps = {
 const ITEMS: TreeViewBaseItem<ExtendedTreeItemProps>[] = [
   {
     id: '1',
-    label: 'Website',
+    label: 'Products',
     children: [
-      { id: '1.1', label: 'Home', color: 'green' },
-      { id: '1.2', label: 'Pricing', color: 'green' },
-      { id: '1.3', label: 'About us', color: 'green' },
       {
-        id: '1.4',
-        label: 'Blog',
+        id: '1.1',
+        label: 'All Products',
+        color: 'green',
         children: [
-          { id: '1.1.1', label: 'Announcements', color: 'blue' },
-          { id: '1.1.2', label: 'April lookahead', color: 'blue' },
-          { id: '1.1.3', label: "What's new", color: 'blue' },
-          { id: '1.1.4', label: 'Meet the team', color: 'blue' },
+          { id: '1.1.1', label: 'Category 1', color: 'blue' },
+          { id: '1.1.2', label: 'Category 2', color: 'blue' },
+          { id: '1.1.3', label: 'Category 3', color: 'blue' },
+          { id: '1.1.4', label: 'Category 4', color: 'blue' },
         ],
       },
+      { id: '1.2', label: 'Add Products', color: 'green' },
+      { id: '1.3', label: 'Edit Products', color: 'green' },
     ],
   },
   {
     id: '2',
-    label: 'Store',
+    label: 'Inventory',
     children: [
       { id: '2.1', label: 'All products', color: 'green' },
       {
@@ -50,8 +50,10 @@ const ITEMS: TreeViewBaseItem<ExtendedTreeItemProps>[] = [
       { id: '2.4', label: 'Sales', color: 'green' },
     ],
   },
-  { id: '4', label: 'Contact', color: 'blue' },
-  { id: '5', label: 'Help', color: 'blue' },
+  { id: '3', label: 'Shipping', color: 'blue' },
+  { id: '4', label: 'User Management', color: 'blue' },
+  { id: '5', label: 'Contact', color: 'blue' },
+  { id: '6', label: 'Help', color: 'blue' },
 ];
 
 const CustomTreeItem = styled(TreeItem)(({ theme }) => ({
@@ -70,7 +72,8 @@ const CustomTreeItem = styled(TreeItem)(({ theme }) => ({
     backgroundColor: theme.palette.primary.dark,
     padding: theme.spacing(0, 1.2),
     ...theme.applyStyles('light', {
-      backgroundColor: alpha(theme.palette.primary.main, 0.25),
+      backgroundColor: alpha(theme.palette.warning.dark, 0.25),
+      color: theme.palette.background.paper,
     }),
     ...theme.applyStyles('dark', {
       color: theme.palette.primary.contrastText,
@@ -81,6 +84,18 @@ const CustomTreeItem = styled(TreeItem)(({ theme }) => ({
     paddingLeft: 18,
     borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
   },
+  [`& .${treeItemClasses.selected} `]: {
+    backgroundColor: theme.palette.warning.dark,
+    color: theme.palette.primary.contrastText,
+    '&:hover': {
+      backgroundColor: theme.palette.warning.dark,
+      color: theme.palette.primary.contrastText,
+    },
+    '&:not(:focus)': {
+      backgroundColor: theme.palette.warning.dark,
+      color: theme.palette.primary.contrastText,
+    },
+  },
   ...theme.applyStyles('light', {
     color: theme.palette.grey[800],
   }),
@@ -89,15 +104,15 @@ const CustomTreeItem = styled(TreeItem)(({ theme }) => ({
 const DashboardNav = () => {
   return (
     <>
-      <Typography component="h2" variant="subtitle1">
-        Product tree
+      <Typography component="h2" variant="subtitle2">
+        Dashboard
       </Typography>
       <RichTreeView
         items={ITEMS}
         aria-label="pages"
-        multiSelect
-        defaultExpandedItems={['1', '1.1']}
-        defaultSelectedItems={['1.1', '1.1.1']}
+        // multiSelect
+        // defaultExpandedItems={['1', '1.1']}
+        // defaultSelectedItems={['1.1', '1.1.1']}
         sx={{
           m: '0 -8px',
           pb: '8px',
