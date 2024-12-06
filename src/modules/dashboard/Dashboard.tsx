5c44c258c830184cdc7c823fa696c6364ed68696
Hyejin Kim <hjqueeen@gmail.com>
2024-12-06 14:58:43 +0100

    Performance

--- src/modules/dashboard/Dashboard.tsx
+++ src/modules/dashboard/Dashboard.tsx
@@ -1,79 +1,28 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid2';
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import ChartUserByCountry from './components/ChartUserByCountry';
-import CustomizedDataGrid from './components/CustomizedDataGrid';
-import PageViewsBarChart from './components/PageViewsBarChart';
-import SessionsChart from './components/SessionsChart';
-import StatCard, { StatCardProps } from './components/StatCard';
-import Copyright from './internals/components/Copyright';
-import HighlightedCard from './components/HighlightedCard';
 
-const data: StatCardProps[] = [
-  {
-    title: 'Users',
-    value: '14k',
-    interval: 'Last 30 days',
-    trend: 'up',
-    data: [
-      200, 24, 220, 260, 240, 380, 100, 240, 280, 240, 300, 340, 320, 360, 340,
-      380, 360, 400, 380, 420, 400, 640, 340, 460, 440, 480, 460, 600, 880, 920,
-    ],
-  },
-  {
-    title: 'Conversions',
-    value: '325',
-    interval: 'Last 30 days',
-    trend: 'down',
-    data: [
-      1640, 1250, 970, 1130, 1050, 900, 720, 1080, 900, 450, 920, 820, 840, 600,
-      820, 780, 800, 760, 380, 740, 660, 620, 840, 500, 520, 480, 400, 360, 300,
-      220,
-    ],
-  },
-  {
-    title: 'Event count',
-    value: '200k',
-    interval: 'Last 30 days',
-    trend: 'neutral',
-    data: [
-      500, 400, 510, 530, 520, 600, 530, 520, 510, 730, 520, 510, 530, 620, 510,
-      530, 520, 410, 530, 520, 610, 530, 520, 610, 530, 420, 510, 430, 520, 510,
-    ],
-  },
-];
+import Copyright from './internals/components/Copyright';
+
+import dashboard_img from '../../assets/picture/dashboard.png';
 
 export const Dashboard = React.memo(() => {
   return (
-    <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1200px' } }}>
-      {/* cards */}
-      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+    <Box sx={{ width: '100%' }} className="flex flex-col p-5 h-full">
+      <Typography component="h2" variant="h6">
         Overview
       </Typography>
-      {/* <Grid
-        container
-        spacing={2}
-        columns={12}
-        sx={{ mb: (theme) => theme.spacing(2) }}
-      >
-        {data.map((card, index) => (
-          <Grid key={index} size={{ xs: 12, sm: 6, lg: 3 }}>
-            <StatCard {...card} />
-          </Grid>
-        ))}
-        <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
-          <HighlightedCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <SessionsChart />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <PageViewsBarChart />
-        </Grid>
-      </Grid> */}
-
+      <Box className="flex grow mt-5 w-full">
+        <img
+          src={dashboard_img}
+          alt="dashboard_img"
+          className="h-auto"
+          style={{
+            objectFit: 'contain',
+            transition: 'opacity 0.3s',
+          }}
+        />
+      </Box>
       <Copyright sx={{ my: 4 }} />
     </Box>
   );
